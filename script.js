d6622507f21607f3ff59fd694d5bdbd08e34b213
Singhfranky1227 <43443712+Singhfranky1227@users.noreply.github.com>
2019-01-03 09:46:53 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$("#StevenFryane").click(Fadeout(){
+ 
+});
+
+
+
+//Spell #2
+
+$("      ").click(function(){
+ 
+});
+
+
+
+//Spell #3
+
+$("      ").click(function(){
+ 
+});
+
+
+
